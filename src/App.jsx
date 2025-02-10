652659c3ff7767197d032b9993ac22738c4b3181
ABHINEKKUMAR <abhinekagarwal25@.com>
2025-02-10 17:49:50 +0530

    add _feature

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import TaskForm from './components/TaskForm';
 const App =() =>{
   return (
     <div className ='app'>
-     <TaskForm></TaskForm>
+     <TaskForm/>
       <main className ='app_main'>
            <section className='task_column'>Section 1</section>
            <section className='task_column'>Section 2</section>
